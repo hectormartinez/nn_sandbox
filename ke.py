199a741236c1ba83ee6ccd9c79cef29c33e886e9
Héctor Martínez <hector.martinez-alonso@inria.fr>
2016-05-24 16:12:58 +0200

    sandbox

---
+++ ke.py
@@ -0,0 +1,16 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+model = Sequential()
+model.add(Dense(1, input_dim=784, activation='softmax'))
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# generate dummy data
+import numpy as np
+data = np.random.random((1000, 784))
+labels = np.random.randint(2, size=(1000, 1))
+
+# train the model, iterating on the data in batches
+# of 32 samples
+model.fit(data, labels, nb_epoch=10, batch_size=32)
