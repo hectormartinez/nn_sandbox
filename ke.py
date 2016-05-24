880b31e7e63ae73044b535efa266611f9dae5c3c
Héctor Martínez <hector.martinez-alonso@inria.fr>
2016-05-24 16:40:58 +0200

    hello

--- ke.py
+++ ke.py
@@ -1,16 +1,17 @@
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+from keras.datasets import imdb
+
+(X_train, y_train), (X_test, y_test) = imdb.load_data()
 model = Sequential()
-model.add(Dense(1, input_dim=784, activation='softmax'))
+model.add(Dense(1, input_dim=len(X_train[0]), activation='softmax'))
 model.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
 # generate dummy data
-import numpy as np
-data = np.random.random((1000, 784))
-labels = np.random.randint(2, size=(1000, 1))
-
 # train the model, iterating on the data in batches
 # of 32 samples
-model.fit(data, labels, nb_epoch=10, batch_size=32)
+model.fit(X_train, y_train, nb_epoch=10, batch_size=32)
+model.evaluate(X_test,y_test)
