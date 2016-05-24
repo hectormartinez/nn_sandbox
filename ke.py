5f11b41eacddc60ca91e5302e46620a86738fb5b
Héctor Martínez <hector.martinez-alonso@inria.fr>
2016-05-24 16:53:43 +0200

    mnist

--- ke.py
+++ ke.py
@@ -1,11 +1,11 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
-from keras.datasets import imdb
+from keras.datasets import mnist
 
 (X_train, y_train), (X_test, y_test) = imdb.load_data()
 model = Sequential()
-model.add(Dense(1, input_dim=len(X_train[0]), activation='softmax'))
+model.add(Dense(1, input_dim=784, activation='softmax'))
 model.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
@@ -13,5 +13,6 @@ model.compile(optimizer='rmsprop',
 # generate dummy data
 # train the model, iterating on the data in batches
 # of 32 samples
+X_train = np.array(X_train)
 model.fit(X_train, y_train, nb_epoch=10, batch_size=32)
 model.evaluate(X_test,y_test)
